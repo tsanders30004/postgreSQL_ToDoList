e872d47d2405b506676f749e9edac010189dcf6b
Tim Sanders <tsanders30004@gmail.com>
2016-07-26 14:05:20 -0400

    26 Jull 2016:  Add, Mark Complete, and Delete Function Correctly

--- todo.py
+++ todo.py
@@ -9,7 +9,7 @@ app = Flask('MyApp')
 
 @app.route('/')
 def home():
-    query = db.query("select id, task, complete from tasks;")
+    query = db.query("select id, task, complete from tasks order by task;")
     return render_template('list.html', title='Task List', tasks_from_database=query.namedresult())
 
 @app.route('/add', methods=['POST'])
@@ -20,9 +20,75 @@ def add():
     db.query(sql_str)
     return redirect('/')
 
+@app.route('/complete', methods=['POST'])
+def complete():
+    my_tasks = request.form
+    print "tasks"
+    print my_tasks
+
+    # need to find out if it is complete or delete
+
+    for t in my_tasks:
+        if t == "complete":
+            mode = "complete"
+        elif t == "delete":
+            mode = "delete"
+
+    print mode
+
+    if mode == "complete":
+        for t in my_tasks:
+            # print "task"
+            # print t
+            sql = "update tasks set complete=true where id=" + t
+            # print "sql"
+            # print sql
+            if t != "complete":
+                db.query(sql)
+
+    if mode == "delete":
+        for t in my_tasks:
+            print "task"
+            print t
+            sql = "delete from tasks where id=" + t
+            print "sql"
+            print sql
+            if t != "delete":
+                db.query(sql)
+
+    return redirect('/')
 
 
-#   Degug Settings
+    # for t in my_tasks:
+    #     print "task"
+    #     print t
+    #
+    #     sql = "update tasks set complete=true where id=" + t
+    #     print "sql"
+    #     print sql
+    #     if t != "complete":
+    #         db.query(sql)
+    # return redirect('/')
+
+
+
+# @app.route('/complete', methods=['POST'])
+# def complete():
+#     my_tasks = request.form
+#     print "tasks"
+#     print my_tasks
+#     for t in my_tasks:
+#         print "task"
+#         print t
+#         # sql = "delete from tasks where id=" + t + ";"
+#         sql = "update tasks set complete=true where id=" + t
+#         print "sql"
+#         print sql
+#         if t != "complete":
+#             db.query(sql)
+#     return redirect('/')
+
+
 app.debug = True
 
 if __name__ == '__main__':
